3be88a20c4e1751eef65b7646b9d74d93f13d49c
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-05-05 08:14:52 -0400

    Update gitignore (why is this also committing the out folder)

--- out/main.js
+++ out/main.js
@@ -1,30 +1,19 @@
 let mod = new Module("DoubleClickPrevent", "Double Click Prevent", "Adds a window after clicking where subsequent ones are ignored. (Only works with mouse buttons)", 0);
 client.getModuleManager().registerModule(mod);
 let optDebounce = mod.addNumberSetting("debounce", "Debounce Time (ms)", "Window where subsequent clicks are ignored", 0, 50, 1, 0);
-let lastValidClickTimeLeft = 0, lastValidClickTimeRight = 0;
+let lastValidClickTime = [0, 0];
 client.on("click", e => {
     if (!mod.isEnabled())
         return;
     if (!e.isDown)
         return;
-    if (e.button == 1) {
-        let clickTimeLeft = Date.now();
-        if (lastValidClickTimeLeft + optDebounce.getValue() > clickTimeLeft) {
-            e.cancel = true;
-        }
-        else {
-            e.cancel = false;
-            lastValidClickTimeLeft = clickTimeLeft;
-        }
+    if (!(e.button == 1 || e.button == 2))
+        return;
+    let clickTime = Date.now();
+    if (lastValidClickTime[e.button - 1] > clickTime) {
+        e.cancel = true;
     }
-    if (e.button == 2) {
-        let clickTimeRight = Date.now();
-        if (lastValidClickTimeRight + optDebounce.getValue() > clickTimeRight) {
-            e.cancel = true;
-        }
-        else {
-            e.cancel = false;
-            lastValidClickTimeRight = clickTimeRight;
-        }
+    else {
+        lastValidClickTime[e.button - 1] = clickTime;
     }
 });
